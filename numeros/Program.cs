b796b7209f286a32ea79671d515197d497b7ed04
LXaburtoM <lxaburto@uam.edu.ni>
2025-08-29 07:45:11 -0600

    primer trabajo

---
+++ numeros/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese un número del 1 al 3:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+switch (numero)
+{
+    case 1:
+        Console.WriteLine("estudia sistemas");
+        break;
+    case 2:
+        Console.WriteLine("estudia medicina");
+        break;
+    case 3:
+        Console.WriteLine("marketing");
+        break;
+    default:
+        Console.WriteLine("Número fuera de rango");
+        break;
+}
