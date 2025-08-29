f30a333e30f55499b892b40fa8ec0558b231f18a
LXaburtoM <lxaburto@uam.edu.ni>
2025-08-29 08:05:43 -0600

    trabajo en clase 2

---
+++ trabajo_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num;
+int suma = 0;
+string entrada;
+Console.WriteLine(" dime un numero:");
+entrada = Console.ReadLine();
+num = Convert.ToInt32(entrada);
+
+while (num > 0)
+{
+    suma += num;
+    Console.WriteLine(" dime un numero:");
+    entrada = Console.ReadLine();
+    num = Convert.ToInt32(entrada);
+}
+Console.WriteLine("la suma es:" + suma);
